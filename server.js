c1e5952a9dce2c710930cb44bafb27e221abfcf3
garret <garretcabello@gmail.com>
2020-08-28 13:16:26 -0400

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require("./routes");
 const app = express();
